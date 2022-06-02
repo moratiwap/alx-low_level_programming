58a26b143721b45f52ca651bf9bcf1c7fb006c68
moratiwap <moratiwapearl@gmail.com>
2022-06-01 23:57:51 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,16 @@
-/*
- *
- *  * File: 5-printf.c
- *
- *   * Auth: Moratiwa Ndiane
- *
- *    */
-
-
-
 #include <stdio.h>
 
-
+/*
+ *
+ *  * main - Prints "with proper grammar, but the outcome is
+ *
+ *   *                a piece of art," followed by a new line.
+ *
+ *    *
+ *
+ *     * Return: Always 0.
+ *
+ *      */
 
 int main(void)
 
