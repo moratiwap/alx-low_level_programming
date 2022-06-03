96202e8eefbe9ce45e876b4ea304e4188592b0c0
moratiwap <moratiwapearl@gmail.com>
2022-06-03 16:28:42 -0700

    Print hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,14 +8,10 @@ int main(void)
 	int n;
 	char ch;
 
-	for (n = 48; n < 58; n++)
-	{
-		putchar(n);
-	}
+	for (n = 0; n < 10; n++)
+		putchar((n % 10) + '0');
 	for (ch = 'a'; ch <= 'f'; ch++)
-	{
 		putchar(ch)
-	}
-	putchar('\n');
-	return (0);
+			putchar('\n');
+	return (0)
 }
