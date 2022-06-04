a276fa57fe39d538f4c673f9bae0793798f0e0a5
moratiwap <moratiwapearl@gmail.com>
2022-06-03 17:16:11 -0700

    Print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,14 +6,14 @@
 int main(void)
 {
 	int n;
-
-	for (n = 48; < 58; n++)
+	
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 		if (n != 57)
 		{
 			putchar(',');
-			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
