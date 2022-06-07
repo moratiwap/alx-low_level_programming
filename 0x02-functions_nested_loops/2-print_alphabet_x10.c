ae24e0d4d8a8d66516c4c75de81d10eb867510c0
moratiwap <moratiwapearl@gmail.com>
2022-06-07 12:18:44 -0700

    Alphabet x10 question

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,11 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- *  print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
- * followed by a new line
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ *followed by a new line
  */
+
 void print_alphabet_x10(void)
+
 {
 
 	char ch;
@@ -40,5 +41,3 @@ void print_alphabet_x10(void)
 			}
 
 }
-
-
