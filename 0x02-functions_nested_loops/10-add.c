9c8be7d9df6b56443285989b937e3a26b752aede
moratiwap <moratiwapearl@gmail.com>
2022-06-08 06:45:18 -0700

    Question ten a plus b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - sum of two numbers
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
