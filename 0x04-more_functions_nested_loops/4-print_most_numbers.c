9de359799127adedf93cc41a641064f0cc1dcd80
moratiwap <moratiwapearl@gmail.com>
2022-06-10 14:50:25 -0700

    More functions and nested loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers -print int between 0 and 9
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+	}
+	_putchat('\n');
+}
