e8096cf94fb7bf7a2cdb598b523f70b6ed23765c
moratiwap <moratiwapearl@gmail.com>
2022-06-16 09:35:32 -0700

    Question two, 2-strncpy.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,6 +12,8 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; src[i] != '\0'; i++)
 		if (i < n)
-			dest[i++] = '\0';
+			dest[i] = src[i];
+	while (i < n)
+		dest[i++] = '\0';
 	return (dest);
 }
