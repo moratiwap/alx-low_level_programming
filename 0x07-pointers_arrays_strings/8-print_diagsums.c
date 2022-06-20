0f0eed13646f10d9bf67c36ac236e4770e769b0f
moratiwap <moratiwapearl@gmail.com>
2022-06-20 14:36:40 -0700

    Question seven

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * print_diagsums - prints sum of two diagonals
+ * @a: the matrix
+ * @size: lenght of array
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1, sum2;
+	int len;
+
+	i = 0;
+	sum1 = sum2 = 0;
+
+	while (i < len)
+	{
+		if (i % (size - 1) == 0 && i > 0)
+			sum2 += *(a + i);
+		if (i % (size + 1) == 0 || i == 0)
+			sum 1 += *(a + i);
+		i++;
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
+
