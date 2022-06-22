eb8bce689d2c0fde4fc1a00636ed9260e3180328
moratiwap <moratiwapearl@gmail.com>
2022-06-22 09:05:15 -0700

    Question extra

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,21 @@
+/**
+ * wildcmp - comare two strings
+ * @s1: string to compare
+ * @s2: string to compare against
+ * Return: 1 if success, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s@) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
+}
