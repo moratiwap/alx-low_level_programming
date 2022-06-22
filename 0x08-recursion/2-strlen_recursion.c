65b550f26c15bd6ffffff7d9963af87d8cf24c92
moratiwap <moratiwapearl@gmail.com>
2022-06-22 07:00:37 -0700

    Question three

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,14 +5,14 @@
  */
 int _strlen_recursion(char *s)
 {
-	int 1;
+	int l;
 
-	1 = 0;
+	l = 0;
 
 	if (*s != 0)
 	{
-		1++;
-		return (1 + _strlen_recursion(s + 1));
+		l++;
+		return (l + _strlen_recursion(s + 1));
 	}
-	return (1);
+	return (l);
 }
