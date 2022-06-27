9fd8625ef7081d630052d69009a3bdee104280fc
moratiwap <moratiwapearl@gmail.com>
2022-06-27 12:35:29 -0700

    Question one

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,7 +19,7 @@ char *argstostr(int ac, char **av)
 			len++;
 		len++;
 	}
-	str = malloc(sizeof(char) * (len + 1))
+	str = malloc(sizeof(char) * (len + 1));
 
 		if (str == NULL)
 			return (NULL);
