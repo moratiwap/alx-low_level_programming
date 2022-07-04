831968b4e61e207b01f120f106801f001e4823a4
moratiwap <moratiwapearl@gmail.com>
2022-07-04 10:48:51 -0700

    Question C structures and typedef.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef FILE_DOG
-#define FILE_DOG
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - a dog's basic info
  * @name: First member
@@ -11,7 +11,10 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
+/**
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
